bd2d59b569180efd91ef41877f6be20e5c0f87e8
Kweku Moses <kweku.moses@hotmail.com>
2021-03-15 22:44:11 +0100

    added functionality to register user in database

---
+++ models/users.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+//* Vi använder mongoose metoden Schema för att skapa en "mall" i vår mongoDB databas som vi fyller ut med hjälp av vår form i register.ejs
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+//*Modellen skapar en kollektion som får namnet av vår första parameter med ett -s ... så userS, eller basketballplayerS osv.
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,10 @@
 var express = require("express");
 var router = express.Router();
 
+const User = require("../models/users");
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+
 /* GET users listing. */
 router.get("/", function (req, res, next) {
   res.send("respond with a resource");
@@ -10,11 +14,13 @@ router.get("/register", function (req, res, next) {
   res.render("register");
 });
 
+//*Här hanterar vi vår form action från register.ejs
 router.post("/register", (request, response) => {
+  //* Request.body innehåller våra inputfält från register.ejs
   const { name, email, password } = request.body;
-
+  //* Vi har en error array för att kunna printa ut eventuella error som uppstår
   let errors = [];
-
+  //*checka att allt ser ut som det ska
   console.log(`Name: ${name}, Email: ${email}, Password: ${password}`);
 
   if (!name || !email || !password) {
@@ -33,6 +39,7 @@ router.post("/register", (request, response) => {
       password,
     });
   } else {
+    //*Vi skapar en User med hjälp av vår model för users som ligger i models.
     const newUser = new User({
       name,
       email,
@@ -56,6 +63,16 @@ router.post("/register", (request, response) => {
 
 router.get("/login", function (req, res, next) {
   res.render("login");
+});
+
+//* Här hanterar vi vår postaction som vi gör på login.ejs
+router.post("/login", (request, response, next) => {
+  //* Om användaren lyckas authentisera sig gör vi en redirect till /dashboard osv
+  passport.authenticate("local", {
+    successRedirect: "/dashboard",
+    failureRedirect: "/users/login",
+    failureFlash: true,
+  })(request, response, next);
 });
 
 module.exports = router;
