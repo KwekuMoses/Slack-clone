7270440a6d5dbe1c236956068045d4eaf68d386e
Kweku Moses <kweku.moses@hotmail.com>
2021-03-15 23:40:36 +0100

    added fileupload, need to fix broken image

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,6 +1,9 @@
 var express = require("express");
 var router = express.Router();
 
+const fileUpload = require("express-fileupload");
+const path = require("path");
+
 /* GET users listing. */
 router.get("/", function (req, res, next) {
   res.render("dashboard");
@@ -12,6 +15,38 @@ router.get("/edit-profile", function (req, res, next) {
 
 router.get("/view-profile", function (req, res, next) {
   res.render("view-profile");
+});
+
+//* Se variabeln file_name
+router.use(
+  fileUpload({
+    createParentPath: true,
+  })
+);
+router.use(express.urlencoded({ extended: true }));
+
+//*För att visa filerna ifrån uploads mappen */
+router.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+//* Hantera det som händer när vi submitar en bildfil
+router.post("/view-profile", (request, response) => {
+  try {
+    if (request.files) {
+      let profile_pic = request.files.profile_pic;
+
+      console.log(profile_pic);
+
+      //* Name är en egenskap som kommer med från express
+      let file_name = `./uploads/${profile_pic.name}`;
+
+      //*mv är en inbyggd metod som hjälper oss ange vart filen ska hamna
+      profile_pic.mv(file_name);
+
+      response.render("view-profile", { images: [file_name] });
+    } else {
+      response.end("<h1>No file uploaded !</h1>");
+    }
+  } catch (error) {}
 });
 
 module.exports = router;
